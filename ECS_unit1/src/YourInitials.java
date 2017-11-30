b01466f672187b75865708cee01ccfd64edd5959
Carter Klugherz <KlugherzC3159@sduhsd.lan>
2017-11-30 10:37:19 -0800

    Finished Step 3

---
+++ ECS_unit1/src/YourInitials.java
@@ -0,0 +1,31 @@
+
+public class YourInitials 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "For the name Carter Stephen Klugherz" );
+		System.out.println( " CCC" );
+		System.out.println( "C   C" );
+		System.out.println( "C" );
+		System.out.println( "C" );
+		System.out.println( "C" );
+		System.out.println( "C   C");
+		System.out.println( " CCC" );
+		
+		System.out.println( " SSS" );
+		System.out.println( "S   S" );
+		System.out.println( "S" );
+		System.out.println( " SSS" );
+		System.out.println( "    S" );
+		System.out.println( "S   S" );
+		System.out.println( " SSS" );
+		
+		System.out.println( "K   K" );
+		System.out.println( "K  K" );
+		System.out.println( "K K" );
+		System.out.println( "KK" );
+		System.out.println( "K K" );
+		System.out.println( "K  K" );
+		System.out.println( "K   K" );
+	}
+}
