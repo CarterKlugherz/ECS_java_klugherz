56361d39e7bb29be79f703445fddb0e5b7c9a5f4
Carter Klugherz <KlugherzC3159@sduhsd.lan>
2017-12-01 11:10:51 -0800

    finished project 2

---
+++ ECS_unit2_klugherz/src/YourSchedule.java
@@ -0,0 +1,28 @@
+
+public class YourSchedule 
+{
+	public static void main( String[] args )
+	{
+		String Per1, Per2, Per3, Per4, BuisnessMath, ComputerScience, English, Yearbook;
+		
+		Per1 = "BuisnessMath";
+	    Per2 = "ComputerScience";
+		Per3 = "English";
+		Per4 = "Yearbook";
+		BuisnessMath = "Hayden";
+		ComputerScience = "Mauro";
+		English = "Fox";
+		Yearbook = "Travasos";
+		
+		System.out.println( "Carters' Class Schedule");	
+		
+		System.out.println( "---------------------------------------------");
+		System.out.println( "| Per 1 |	" + Per1 + "     |  Hayden 	 |");
+		System.out.println( "| Per 2 |	" + Per2 + "     |  Mauro    |");
+		System.out.println( "| Per 3 |	" + Per3 + "     |  Fox 	 |");
+		System.out.println( "| Per 4 |	" + Per4 + "     |  Travasos |");
+		System.out.println( "---------------------------------------------");
+		
+		
+	}
+}
