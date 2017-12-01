f556391ce88323b9ff62cae32cb0456fdfa44643
Carter Klugherz <KlugherzC3159@sduhsd.lan>
2017-12-01 10:00:31 -0800

    Finished step 2 of the second project

---
+++ ECS_unit2_klugherz/src/UsingVariables.java
@@ -0,0 +1,10 @@
+
+public class UsingVariables 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "This is room # " + 113 + ".");
+		System.out.println( "e is close to " + 2.71828 + ".");
+		System.out.println( "I am learning a bit about \"Computer Science\"");
+	}
+}
