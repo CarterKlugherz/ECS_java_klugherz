ecc2d48f95c04c235ea4a0562d76f9b6835b95b9
Carter Klugherz <KlugherzC3159@sduhsd.lan>
2017-12-01 10:36:13 -0800

    Finished step 4

---
+++ ECS_unit2_klugherz/src/StillUsingVariables.java
@@ -0,0 +1,14 @@
+
+public class StillUsingVariables 
+{
+	public static void main( String[] args )
+	{
+		String myName;
+		int myGradYear;
+		
+		myName = "Carter S. Klugherz";
+		myGradYear = 2018;	// that's this year
+		
+		System.out.println( "My name is " + myName + " and I'll graduate in " + myGradYear + " .");
+	}
+}
