a6f56e4ff97cad1f238d9fc758ac53e5b3e25978
Carter Klugherz <KlugherzC3159@sduhsd.lan>
2017-12-05 09:41:13 -0800

    finished the first step of the keyboard assignment

--- ECS_Keyboard_Inputs/src/AskingQuestions.java
+++ ECS_Keyboard_Inputs/src/AskingQuestions.java
@@ -1,35 +1,26 @@
+package KeyboardInputs;
+
 import java.util.Scanner;
 
-public class AskingQuestions
+public class NameAgeSalary
 {
 	public static void main( String[] args )
 	{
 		Scanner keyboard = new Scanner(System.in);
-		
 		String name;
-		int age, feet, inches;
-		double weight;
+		int age, salary;
 		
-		System.out.println("What is your name?" );
+		System.out.println("Hello, what is your name?" );
 		name = keyboard.next();
 		
-		System.out.println("How old are you?" );
+		System.out.println("Hi " +name+ " How old are you?" );
 		age = keyboard.nextInt();
+	
+		System.out.println("So you are " + age + "? " I guess that's not too old, " + name + "." How much do you make per hour?" );
+		salary = keyboard.nextInt();
 		
-		System.out.println("How many feet tall are you?" );
-		feet = keyboard.nextInt();
+		System.out.println("Wow, " + salary + " is a lot for your age!" );
 
-		System.out.println("How many inches?" );
-		inches = keyboard.nextInt();
-		
-		System.out.println("How much do you weigh?" );
-		weight = keyboard.nextDouble();
-		
-		System.out.println("What is your name" );
-		name = keyboard.next();
-		
-		System.out.println("So you're name is " + name + ", you are " + age + " years old, you're " + feet + "'" + inches + " , and you weigh " + weight + " pounds." );
-		
 		keyboard.close();
 	}
 }
